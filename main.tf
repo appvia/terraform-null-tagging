4088377c884b8daef7d2b27d215a83bfc26538eb
Rohith Jayawardene <gambol99@gmail.com>
2024-05-22 17:19:14 +0100

    fix: the name on the tag was incorrect

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ locals {
     var.application_tier != "" ? { "Tier" = var.application_tier } : {},
     var.cost_center != "" ? { "CostCenter" = var.cost_center } : {},
     var.data_classification != "" ? { "DataClassification" = var.data_classification } : {},
-    var.environment != "" ? { "Enviroment" = var.environment } : {},
+    var.environment != "" ? { "Environment" = var.environment } : {},
     var.git_repo != "" ? { "GitRepo" = var.git_repo } : {},
     var.owner != "" ? { "Owner" = var.owner } : {},
     var.product != "" ? { "Product" = var.product } : {},
